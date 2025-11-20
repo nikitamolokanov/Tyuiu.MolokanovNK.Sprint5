bb3eb7bb63ea6dc8b302dae296508cc5371df13d
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-11-20 18:23:48 +0500

    Add project files.

---
+++ Tyuiu.MolokanovNK.Sprint5.Task0.V18.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MolokanovNK.Sprint5.Task0.V18.Lib
+{
+    public class DataService : ISprint5Task0V18
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double z = x / Math.Sqrt(Math.Pow(x, 2) + x);
+            z = Math.Round(z, 3);
+            File.WriteAllText(path, Convert.ToString(z));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint5.Task0.V18/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.MolokanovNK.Sprint5.Task0.V18.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #5 | Выполнил: Молоканов Н. К. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Работа с файлами                                                  *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #18                                                             *");
+Console.WriteLine("* Выполнил: Молоканов Никита Константинович | ИБКСб-25-1                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дано выражение             . Вычислить значение при x = 3,              *");
+Console.WriteLine("* результат сохранить в файл и вывести на консоль.                        *");
+Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int x = 3;
+Console.WriteLine($"x = {x}");
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string result = ds.SaveToFileTextData(x);
+
+Console.WriteLine("Файл:" + result);
+Console.WriteLine("Создан!");
+
+
+Console.ReadKey();
