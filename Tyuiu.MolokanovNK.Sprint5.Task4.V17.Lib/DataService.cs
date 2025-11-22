48256e82088b86a9774198e7ea8148d723ef6af5
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-11-22 21:38:50 +0500

    Task4

---
+++ Tyuiu.MolokanovNK.Sprint5.Task4.V17.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MolokanovNK.Sprint5.Task4.V17.Lib
+{
+    public class DataService : ISprint5Task4V17
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+
+            double x = 4.68;
+            double res = Math.Round(Math.Sin(2/ 3*x) + Math.Pow(x,2),3);
+           
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint5.Task4.V17/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.MolokanovNK.Sprint5.Task4.V17.Lib;
+
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #5 | Выполнил: Молоканов Н. К. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Работа с файлами                                                  *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #18                                                             *");
+Console.WriteLine("* Выполнил: Молоканов Никита Константинович | ИБКСб-25-1                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дана функция, ( F(x) = {cos(x)}/{x-0.4} + sin(x) * 8x + 2 )             *");
+Console.WriteLine("* (произвести табулирование) f(x) на заданном диапазоне [-5; 5] с шагом 1.*");
+Console.WriteLine("* Произвести проверку деления на ноль.                                    *");
+Console.WriteLine("* При делении на ноль вернуть значение 0.                                 *");
+Console.WriteLine("* результат сохранить в текстовый файл OutPutFileTask1.txt                *");
+Console.WriteLine("* и вывести на консоль в таблицу. Значения округлить до двух знаков после запятой.*");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask4V17.txt ";
+
+Console.WriteLine("Данные находятся в файле: " + path);
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+double res = ds.LoadFromDataFile(path);
+Console.WriteLine("Результат вычисления: " + res);
+Console.ReadKey();
+
+
+
+
+Console.ReadKey();
